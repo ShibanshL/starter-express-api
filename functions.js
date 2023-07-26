cbe750a8a43021521fdc7c9779691572598ce9b8
ShibanshL <shibansh.w@gmail.com>
2023-07-26 16:57:29 +0530

    fifteenth commit

--- functions.js
+++ functions.js
@@ -1,9 +1,17 @@
-const login = async (req, res) => {
-  console.log(req.body);
-  res.status(201).json({ message: "OK" });
-};
-
 const registerData = require("./Model/Register");
+
+const login = async (req, res) => {
+  try {
+    const registerCheck = await registerData.findOne({ name: req.body.name });
+    if (!registerCheck) {
+      return res.status(400).json({ message: "No such user exist" });
+    }
+    const newUser = await registerData.findOne({ name: req.body.name });
+    res.status(201).json({ id: newUser._id });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
 
 const register = async (req, res) => {
   try {
